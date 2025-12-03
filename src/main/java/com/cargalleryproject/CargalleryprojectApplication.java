d07cf95f0ed4fc783e14d68f70b597e0569f3a30
TalhaKocc <talhakocc1834@gmail.com>
2025-12-03 18:41:09 +0300

    Initial commit

---
+++ src/main/java/com/cargalleryproject/CargalleryprojectApplication.java
@@ -0,0 +1,13 @@
+package com.cargalleryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargalleryprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargalleryprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cargalleryproject/CargalleryprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cargalleryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargalleryprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
