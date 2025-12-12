6aa7e388ba174b5bce7744d80b3652bf48a5dd09
TalhaKocc <talhakocc1834@gmail.com>
2025-12-12 19:07:09 +0300

    hatalı girişi engellemek için validation eklendi

--- src/main/java/com/talhakoc/dto/user/request/LoginRequestDto.java
+++ src/main/java/com/talhakoc/dto/user/request/LoginRequestDto.java
@@ -1,5 +1,7 @@
 package com.talhakoc.dto.user.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +13,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class LoginRequestDto {
 
+	@Email(message = "Lütfen eamil formatında giriniz")
+	@NotBlank(message = "Bu alan boş bırakılamaz")
 	private String email;
 	
+	@NotBlank(message = "Bu alan boş bırakılamaz")
 	private String password;
 
 }
