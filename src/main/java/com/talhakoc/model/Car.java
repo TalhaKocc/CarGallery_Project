903e61f46985c54c95512c201fddb2cf560ae967
TalhaKocc <talhakocc1834@gmail.com>
2025-12-30 20:15:23 +0300

    model package i içinde değişiklikler yapıldı

--- src/main/java/com/talhakoc/model/Car.java
+++ src/main/java/com/talhakoc/model/Car.java
@@ -2,15 +2,7 @@ package com.talhakoc.model;
 
 import java.math.BigDecimal;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -38,15 +30,15 @@ public class Car {
 	@Column(name = "car_status")
 	private CarStatus status;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private CarMake carMake;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private CarModel carModel;
 	    
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private CarEngine carEngine;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private CarFuel carFuel;
 }
--- src/main/java/com/talhakoc/model/User.java
+++ src/main/java/com/talhakoc/model/User.java
@@ -1,14 +1,6 @@
 package com.talhakoc.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -35,8 +27,7 @@ public class User {
 	@Column(nullable = false)
 	private String password;
 	
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "role_id", nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
 	
 	@Column(nullable = false)
